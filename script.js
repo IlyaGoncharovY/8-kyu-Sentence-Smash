c474ab723f5b9b30ca23101c3f106c5c3b73f55f
IlyaGoncharovY <goncharov.i@rambler.ru>
2022-01-04 01:16:15 +0300

    test ok

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 function smash (words) {
-    // Write a function that takes an array of words and 
-    // smashes them together into a sentence and returns 
-    // the sentence. You can ignore any need to sanitize words or add punctuation, 
-    // but you should add spaces between each word. Be careful, there shouldn't 
-    // be a space at the beginning or the end of the sentence!
+    return words.join(" ");
  };
